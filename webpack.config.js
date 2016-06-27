b1af5a6fd42482468d734d0162d23c2443d30973
ideascomealive <jim@ideascomealive.com>
2016-06-27 14:33:38 -0500

    base ui

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/main',
+  output: { 
+    path: path.join(__dirname, '/dist'), 
+    publicPath: '/',
+    filename: 'bundle.js' }, 
+  module: {
+    loaders: [
+      // JAVASCRIPT
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      // SASS
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+]
+};
